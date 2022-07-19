b1b62238b640d90989cc1ea9235b81c138af5ed9
Darrell Root <feedback@networkmom.net>
2022-07-19 16:45:28 -0700

    First working SNMPv3 test decode!

--- Sources/SwiftSnmpKit/SnmpV3Message.swift
+++ Sources/SwiftSnmpKit/SnmpV3Message.swift
@@ -35,7 +35,7 @@ public struct SnmpV3Message: AsnData {
     private var flagsAsn: AsnValue {
         return AsnValue.init(octetStringData: Data([flagsOctet]))
     }
-    private let messageSecurityModel: UInt8 = 3
+    private var messageSecurityModel: UInt8 = 3
 
     private var engineId: Data // example: 80000009034c710c19e30d
     private var engineIdAsn: AsnValue {
@@ -109,6 +109,154 @@ public struct SnmpV3Message: AsnData {
     
     internal var asnData: Data {
         return asn.asnData
+    }
+    
+    /// Creates SNMPv3 message data structure from the data encapsulated inside a UDP SNMP reply.
+    ///
+    /// Takes data from a SNMP reply and uses it to create a SNMP message data structure.  Returns nil if the data cannot form a complete SNMP reply data structure.
+    /// This initializer is not designed for creating a SNMP message for transmission.
+    /// - Parameter data: The network contents of a UDP reply, with the IP and UDP headers already stripped off.
+    public init?(data: Data) {
+        guard let outerSequence = try? AsnValue(data: data) else {
+            SnmpError.log("Outer ASN is not a sequence")
+            return nil
+        }
+        guard case .sequence(let contents) = outerSequence else {
+            SnmpError.log("Unable to extract AsnValues")
+            return nil
+        }
+        guard contents.count == 4 else {
+            SnmpError.log("Expected 4 contents in SNMPv3, found \(contents.count)")
+            return nil
+        }
+        guard case .integer(let snmpVersionInteger) = contents[0] else {
+            SnmpError.log("Expected AsnInteger, got \(contents[0])")
+            return nil
+        }
+        guard let snmpVersion = SnmpVersion(rawValue: Int(snmpVersionInteger)) else {
+            SnmpError.log("Received invalid SNMP Version \(snmpVersionInteger)")
+            return nil
+        }
+        self.version = snmpVersion
+        
+        guard case .sequence(let msgGlobalData) = contents[1] else {
+            SnmpError.log("Expected message global data, got \(contents[1])")
+            return nil
+        }
+        guard msgGlobalData.count == 4 else {
+            SnmpError.log("Expected message global data count, got \(msgGlobalData)")
+            return nil
+        }
+        guard case .integer(let msgId) = msgGlobalData[0] else {
+            SnmpError.log("Expected messageId integer \(msgGlobalData[0])")
+            return nil
+        }
+        self.messageId = Int32(msgId)
+        guard case .integer(let maxSize) = msgGlobalData[1] else {
+            SnmpError.log("Expected messageId integer got \(msgGlobalData[1])")
+            return nil
+        }
+        self.maxSize = Int(maxSize)
+        guard case .octetString(let flags) = msgGlobalData[2] else {
+            SnmpError.log("Expected messageId octetString got \(msgGlobalData[2])")
+            return nil
+        }
+        guard flags.count == 1 else {
+            SnmpError.log("Expected 1 byte of messageFlags  got \(flags)")
+            return nil
+        }
+        let flagsInt = UInt8(flags[flags.startIndex])
+        if ((flagsInt & 0b00000100) > 0) {
+            self.reportable = true
+        } else {
+            self.reportable = false
+        }
+        if ((flagsInt & 0b00000010) > 0) {
+            self.encrypted = true
+        } else {
+            self.encrypted = false
+        }
+        if ((flagsInt & 0b00000001) > 0) {
+            self.authenticated = true
+        } else {
+            self.authenticated = false
+        }
+        guard case .integer(let messageSecurityModel) = msgGlobalData[3] else {
+            SnmpError.log("Expected messageSecurityModel integer got \(msgGlobalData[3])")
+            return nil
+        }
+        guard messageSecurityModel < 256 && messageSecurityModel >= 0 else {
+            SnmpError.log("Invalid messageSecurityModel \(messageSecurityModel)")
+            return nil
+        }
+        self.messageSecurityModel = UInt8(messageSecurityModel)
+        
+        guard case .octetString(let securityParametersData) = contents[2] else {
+            SnmpError.log("Expected octetString got \(contents[2])")
+            return nil
+        }
+        guard case .sequence(let securityParameters) = try? AsnValue(data: securityParametersData) else {
+            SnmpError.log("Expected security parametrs sequence got \(securityParametersData)")
+            return nil
+        }
+        guard case .octetString(let engineIdOctets) = securityParameters[0] else {
+            SnmpError.log("Expected security parametrs octetString got \(securityParameters[0])")
+            return nil
+        }
+        self.engineId = engineIdOctets
+        guard case .integer(let engineBoots) = securityParameters[1] else {
+            SnmpError.log("Expected engineBoots Integer got \(securityParameters[1])")
+            return nil
+        }
+        self.engineBoots = Int(engineBoots)
+        guard case .integer(let engineTime) = securityParameters[2] else {
+            SnmpError.log("Expected engineTime Integer got \(securityParameters[2])")
+            return nil
+        }
+        self.engineTime = Int(engineTime)
+        guard case .octetString(let usernameData) = securityParameters[3] else {
+            SnmpError.log("Expected username octetString got \(securityParameters[3])")
+            return nil
+        }
+        // utf8 string decoding should never fail
+        self.userName = String(data: usernameData, encoding: .utf8)!
+        guard case .octetString(let msgAuthenticationParametersData) = securityParameters[4] else {
+            SnmpError.log("Expected msgAuthenticationParametersData octetString got \(securityParameters[4])")
+            return nil
+        }
+        #warning("do something with message authentication and privacy parameters")
+        guard case .octetString(let msgPrivacyParametersData) = securityParameters[5] else {
+            SnmpError.log("Expected msgPrivacyParametersData octetString got \(securityParameters[5])")
+            return nil
+        }
+        guard case .sequence(let msgData) = contents[3] else {
+            SnmpError.log("Expected msgData sequence got \(contents[3])")
+            return nil
+        }
+        guard msgData.count == 3 else {
+            SnmpError.log("Expected msgData sequence size 2 got \(msgData.count)")
+            return nil
+        }
+        guard case .octetString(let engineId2Data) = msgData[0] else {
+            SnmpError.log("Expected engineId2 octetString 2 got \(msgData[0])")
+            return nil
+        }
+        guard engineId2Data == engineIdOctets else {
+            SnmpError.log("engineIds do not match! \(engineId2Data) \(engineIdOctets)")
+            return nil
+        }
+        guard case .octetString(let contextNameData) = msgData[1] else {
+            SnmpError.log("contextName expected octetString got \(msgData[1])")
+            return nil
+        }
+        switch msgData[2] {
+        case .snmpResponse(let response):
+            self.snmpPdu = response
+        
+        default:
+            SnmpError.log("Expected SNMP response PDU, got \(msgData[2])")
+            return nil
+        }
     }
     
     
--- Tests/SwiftSnmpKitTests/SnmpV3Tests.swift
+++ Tests/SwiftSnmpKitTests/SnmpV3Tests.swift
@@ -27,7 +27,50 @@ class SnmpV3Tests: XCTestCase {
         //XCTAssert(data[0..<9] == "306402010330110204".hexstream!)
         print(data.hexdump)
     }
-
+    /*
+     Simple Network Management Protocol
+         msgVersion: snmpv3 (3)
+         msgGlobalData
+             msgID: 47813554
+             msgMaxSize: 1400
+             msgFlags: 00
+                 .... .0.. = Reportable: Not set
+                 .... ..0. = Encrypted: Not set
+                 .... ...0 = Authenticated: Not set
+             msgSecurityModel: USM (3)
+         msgAuthoritativeEngineID: 80000009034c710c19e30d
+             1... .... = Engine ID Conformance: RFC3411 (SNMPv3)
+             Engine Enterprise ID: ciscoSystems (9)
+             Engine ID Format: MAC address (3)
+             Engine ID Data: MAC address: Cisco_19:e3:0d (4c:71:0c:19:e3:0d)
+         msgAuthoritativeEngineBoots: 0
+         msgAuthoritativeEngineTime: 0
+         msgUserName: ciscouser
+         msgAuthenticationParameters: <MISSING>
+         msgPrivacyParameters: <MISSING>
+         msgData: plaintext (0)
+             plaintext
+                 contextEngineID: 80000009034c710c19e30d
+                 contextName:
+                 data: get-response (2)
+                     get-response
+                         request-id: 47813554
+                         error-status: noError (0)
+                         error-index: 0
+                         variable-bindings: 1 item
+                             1.3.6.1.2.1.1.1.0: "SG250-08 8-Port Gigabit Smart Switch"
+                 [Response To: 1]
+                 [Time: 0.004505000 seconds]
+     */
+    func testSnmpV31() throws {
+        let data = "30818e0201033010020402d993b20202057804010002010304243022040b80000009034c710c19e30d0201000201000409636973636f75736572040004003051040b80000009034c710c19e30d0400a240020402d993b20201000201003032303006082b06010201010100042453473235302d303820382d506f7274204769676162697420536d61727420537769746368".hexstream!
+        guard let snmpMessageV3 = SnmpV3Message(data: data) else {
+            XCTFail()
+            return
+        }
+        XCTAssert(snmpMessageV3.messageId == 47813554)
+        XCTAssert(snmpMessageV3.version == .v3)
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
